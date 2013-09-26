3d59a14830eb8fd628acda28b0c2b870a6621c44
Emerson Rocco Bernardino <github@dev2dev.com.br>
2013-09-26 06:39:54 -0300

    funcional 2

--- gen/br/com/hackpontocerto/R.java
+++ gen/br/com/hackpontocerto/R.java
@@ -28,13 +28,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f090008;
         public static final int editText1=0x7f090002;
         public static final int mainLayout=0x7f090000;
+        public static final int spinner1=0x7f090006;
         public static final int spinnerSeg=0x7f090001;
+        public static final int textView1=0x7f090005;
         public static final int textView2=0x7f090003;
         public static final int textView3=0x7f090004;
-        public static final int webView1=0x7f090005;
+        public static final int webView1=0x7f090007;
     }
     public static final class layout {
         public static final int tela_mapa=0x7f030000;
